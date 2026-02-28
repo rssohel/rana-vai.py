d09d59d6f8ea638fa7006008a3623e1dd7f060e9
rssohel <143024387+rssohel@users.noreply.github.com>
2026-02-28 15:47:41 +0400

    Remove unnecessary echo statements in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-echo "Installing requirements..."
-
 pkg update -y
 pkg install python -y
 pip install -r requirements.txt
-
-echo "Installation Complete!"
+echo "Installation complete!"
